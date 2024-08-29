99eb5c29e87c7bcf7f2e537ab4f3b9cde010df0c
Ayush Thakur <ayt12321232@gmail.com>
2024-08-29 19:13:59 +0530

    Add cors package

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express')
+var cors = require('cors')
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function run(prompt) {
   try {
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(prompt + " and give the answer in simple language");
     const response = result.response;
     const text = response.text();
     return text;
@@ -16,7 +17,8 @@ async function run(prompt) {
 }
 
 const app = express()
-const port = process.env.PORT || 4000;
+app.use(cors())
+const port = process.env.PORT || 3000;
 
 let text;
 
